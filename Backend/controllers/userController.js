13d5eb57a9642a20caeba188040f5a3662de7691
rishabh2005-cpu <jainr4707@gmail.com>
2024-12-30 22:47:08 +0530

    comitted

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -6,7 +6,24 @@ import validator from "validator"
 
 //login user
 const loginUser = async (req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+        if (!user) {
+           return res.json({success:false,message:"User Doesn't exist"})
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if (!isMatch) {
+            return res.json({success:false,message:"invalid credentials"});
+        }
+        const token = createToken(user._id);
+        res.json({success:true,token})
 
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+        
+    }
 }
 const createToken = (id)=>{
     return jwt.sign({id},process.env.JWT_SECRET)
@@ -25,7 +42,7 @@ try {
     if (!validator.isEmail(email)) {
         return res.json({success:false,message:"Please enter a valid email"})
     }
-    if (password<8) {
+    if (password.length<8) {
         return res.json({success:false,message:"please enter a strong password"})
     }
     // hashing user password
